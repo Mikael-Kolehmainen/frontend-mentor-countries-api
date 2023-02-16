9e8da59024ded6a214db89cb524db32742cd07d9
Mikael-Kolehmainen-School <mikaelkolehmainen05@gmail.com>
2023-02-16 14:29:30 +0200

    Started working on filter front-end

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,9 +3,15 @@
 export default function Home() {
     return (
         <section>
-            <div className='search-bar'>
-                <i className='fa-solid fa-magnifying-glass'></i>
-                <input type='text' placeholder='Search for a country...' />
+            <div className='search'>
+                <div className='search-bar'>
+                    <i className='fa-solid fa-magnifying-glass'></i>
+                    <input type='text' placeholder='Search for a country...' />
+                </div>
+                <select className='continent'>
+                    <option value='africa'>Africa</option>
+                    <option value='america'>America</option>
+                </select>
             </div>
         </section>
     );
