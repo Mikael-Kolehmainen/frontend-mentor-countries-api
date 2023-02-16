6ab282fc26641aff85c07beb6f63cb448b98966d
Mikael-Kolehmainen-School <mikaelkolehmainen05@gmail.com>
2023-02-16 14:24:03 +0200

    front-end for search bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import './styles/css/main.css';
 import Header from './components/Header';
+import Home from './components/Home';
 
 export default function App() {
   return (
     <div className="App">
       <Header />
-      <section>
-        <p>Test</p>
-      </section>
+      <Home />
     </div>
   );
 }
---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+    return (
+        <section>
+            <div className='search-bar'>
+                <i className='fa-solid fa-magnifying-glass'></i>
+                <input type='text' placeholder='Search for a country...' />
+            </div>
+        </section>
+    );
+};
