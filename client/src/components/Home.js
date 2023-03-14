dcc13e21ba124b233ee5c467e285036ef8d53bed
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2023-03-14 20:10:40 +0200

    Worked on search front-end

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,18 +1,22 @@
 
 
 export default function Home() {
-    return (
-        <section>
-            <div className='search'>
-                <div className='search-bar'>
-                    <i className='fa-solid fa-magnifying-glass'></i>
-                    <input type='text' placeholder='Search for a country...' />
-                </div>
-                <select className='continent'>
-                    <option value='africa'>Africa</option>
-                    <option value='america'>America</option>
-                </select>
-            </div>
-        </section>
-    );
+  return (
+    <section>
+      <div className='search'>
+        <div className='search-bar'>
+          <i className='fa-solid fa-magnifying-glass'></i>
+          <input type='text' placeholder='Search for a country...' />
+        </div>
+        <select className='region'>
+          <option selected disabled>Filter by Region</option>
+          <option value='africa'>Africa</option>
+          <option value='america'>America</option>
+          <option value='asia'>Asia</option>
+          <option value='europe'>Europe</option>
+          <option value='oceania'>Oceania</option>
+        </select>
+      </div>
+    </section>
+  );
 };
