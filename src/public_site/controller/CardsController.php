e40d5d2b9eb55b21146b17cb8f36f356de647b41
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2023-04-23 21:26:50 +0300

    Connected to countries api

--- src/public_site/controller/CardsController.php
+++ src/public_site/controller/CardsController.php
@@ -6,6 +6,13 @@ class CardsController
 {
   public function showCards(): void
   {
+    $countries = $this->getCountries();
+
+    foreach ($countries as $country) {
+      $name = $country->name->common;
+      echo "<p>$name</p>";
+    }
+
     echo "
       <div class='cards'>
         <a href='index.php/details' class='card'>
@@ -45,5 +52,34 @@ class CardsController
         </a>
       </div>
     ";
+  }
+
+  private function getCountries(): array
+  {
+    $curl = curl_init();
+
+    $url = "https://restcountries.com/v3.1/all";
+
+    curl_setopt_array($curl, [
+      CURLOPT_URL => $url,
+      CURLOPT_HTTPHEADER => [
+        "Content-type: text/plain",
+      ],
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "GET",
+    ]);
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+
+    $countries = json_decode($response);
+
+    return $countries;
   }
 }
